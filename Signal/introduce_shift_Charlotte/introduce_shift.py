838f52d73e9925075d084f3fe9cc07eafca07d61
Amrutha Krishna <amruthakrishna.unni@gmail.com>
2024-07-26 10:22:53 +0200

    added norm variable for the new pdf with _shift handle so that t2w will pick it up correctly + other smaller changes

--- Signal/introduce_shift_Charlotte/introduce_shift.py
+++ Signal/introduce_shift_Charlotte/introduce_shift.py
@@ -7,16 +7,18 @@ def get_options():
   parser = OptionParser()
   parser.add_option('--inputWS', dest='inputWS', default='', help='root file containing no-interference signal WS')
   parser.add_option('--cat', dest='cat', default='', help='category (eg: UntaggedTag_0)')
-  parser.add_option('--year', dest='year', default='2016', help='Year')
+  parser.add_option('--year', dest='year', default='2018', help='Year')
   return parser.parse_args()
 
 (opt,args) = get_options()
 
 f=ROOT.TFile(opt.inputWS, "read")
 w = f.Get("wsig_13TeV")
-w.Print()
+#w.Print()
 
-dm_original = w.function("dm_dcb_HHggTauTaukl1_2016_SR1_13TeV")
+#dm_original = w.function("dm_dcb_HHggTauTaukl1_2016_SR1_13TeV")
+dm_original_name = "dm_dcb_GG2H_%s_%s_13TeV"%(opt.year, opt.cat)
+dm_original = w.function(dm_original_name)
 
 GammaH = ROOT.RooRealVar("GammaH", "GammaH", 0.0)
 
@@ -41,25 +43,39 @@ alpha_dict = {
 # create spline for GammaH dependence
 width_ratio = np.linspace(0, 30, 300)
 alpha = alpha_dict['%s'%(opt.cat)]
-mass_shift = 0.001 * alpha * np.sqrt(width_ratio)  # factor of 10e-3 to convert MeV to GeV
+mass_shift = 0.001 * alpha * np.sqrt(width_ratio)  # factor of 10e-3 to convert alpha from MeV to GeV
 width_spline = ROOT.RooSpline1D("width_spline", "width_spline", GammaH, len(width_ratio), width_ratio, mass_shift)
 
 # just test the spline works
 for xi in width_ratio:
  GammaH.setVal(xi)
- width_spline.Print()
+ #width_spline.Print()
 
 # create new dm shift which is MH + GammaH dependence
-dm_shift = ROOT.RooFormulaVar("dm_shift_dcb_HHggTauTaukl1_2016_SR1_13TeV", "dm_shift_HHggTauTaukl1_2016_SR1_13TeV", "@0 + @1", ROOT.RooArgList(dm_original, width_spline))
+#dm_shift = ROOT.RooFormulaVar("dm_shift_dcb_HHggTauTaukl1_2016_SR1_13TeV", "dm_shift_HHggTauTaukl1_2016_SR1_13TeV", "@0 + @1", ROOT.RooArgList(dm_original, width_spline))
+dm_shift_name = "dm_shift_dcb_GG2H_%s_%s_13TeV"%(opt.year, opt.cat)
+dm_shift = ROOT.RooFormulaVar(dm_shift_name, dm_shift_name, "@0 + @1", ROOT.RooArgList(dm_original, width_spline))
 imp = getattr(w, "import")
 imp(dm_shift, ROOT.RooFit.RecycleConflictNodes())
 
-# use w.factory to create a new signal model with GammaH dependence included
-original_model_name = "hggpdfsmrel_HHggTauTaukl1_2016_SR1_13TeV"
-new_model_name = "hggpdfsmrel_shift_HHggTauTaukl1_2016_SR1_13TeV"
+# change also the norm function name 
+original_norm_func = w.function("dcb_GG2H_%s_%s_13TeV_norm"%(opt.year, opt.cat))
+new_norm_func = original_norm_func.Clone("dcb_shift_GG2H_%s_%s_13TeV_norm"%(opt.year, opt.cat))
+imp = getattr(w,"import")
+imp(new_norm_func, ROOT.RooFit.RecycleConflictNodes())
 
-original_dm_name = "dm_dcb_HHggTauTaukl1_2016_SR1_13TeV"
-new_dm_name = "dm_shift_dcb_HHggTauTaukl1_2016_SR1_13TeV"
+# use w.factory to create a new signal model with GammaH dependence included
+#original_model_name = "hggpdfsmrel_HHggTauTaukl1_2016_SR1_13TeV"
+#new_model_name = "hggpdfsmrel_shift_HHggTauTaukl1_2016_SR1_13TeV"
+
+#original_dm_name = "dm_dcb_HHggTauTaukl1_2016_SR1_13TeV"
+#new_dm_name = "dm_shift_dcb_HHggTauTaukl1_2016_SR1_13TeV"
+
+original_model_name = "dcb_GG2H_%s_%s_13TeV"%(opt.year, opt.cat)
+new_model_name = "dcb_shift_GG2H_%s_%s_13TeV"%(opt.year, opt.cat)
+
+original_dm_name = "dm_dcb_GG2H_%s_%s_13TeV"%(opt.year, opt.cat)
+new_dm_name = "dm_shift_dcb_GG2H_%s_%s_13TeV"%(opt.year, opt.cat)
 
 factory_string = "EDIT::%s(%s, %s=%s )"%(new_model_name, original_model_name, original_dm_name, new_dm_name)
 print(factory_string)
@@ -72,6 +88,8 @@ w.Print()
 # how does that change as a function of GammaH?
 for xi in width_ratio:
   w.var("GammaH").setVal(xi)
-  print(xi, w.function("mean_dcb_HHggTauTaukl1_2016_SR1_13TeV_hggpdfsmrel_shift_HHggTauTaukl1_2016_SR1_13TeV").getVal())
+  #print(xi, w.function("mean_dcb_HHggTauTaukl1_2016_SR1_13TeV_hggpdfsmrel_shift_HHggTauTaukl1_2016_SR1_13TeV").getVal())
+  print(xi, w.function("mean_dcb_GG2H_%s_%s_13TeV_dcb_shift_GG2H_%s_%s_13TeV"%(opt.year, opt.cat, opt.year, opt.cat)).getVal())
+  
 
-w.writeToFile("new_model.root")
+w.writeToFile("CMS-HGG_sigfit_GG2H_%s_%s_width_model_novtxsplit_syst.root"%(opt.year, opt.cat))
